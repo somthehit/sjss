24db08d24898d5dd132469f9d1edc9a7f5b9f555
somthehit <somthehit@gmail.com>
2026-05-28 17:38:22 +0545

    Fix allowtransparency: use string instead of bool

--- app/page.tsx
+++ app/page.tsx
@@ -308,7 +308,7 @@ export default async function HomePage() {
           </div>
 
           <div className="mt-8 flex justify-center">
-            <iframe src="https://www.ashesh.com.np/calendar-widget/calendar.php?tithi=1&header_color=default&api=872156q120" frameBorder="0" scrolling="no" marginWidth={0} marginHeight={0} style={{ border: 'none', overflow: 'hidden', width: 370, height: 333, borderRadius: 5 }} allowtransparency={true} />
+            <iframe src="https://www.ashesh.com.np/calendar-widget/calendar.php?tithi=1&header_color=default&api=872156q120" frameBorder="0" scrolling="no" marginWidth={0} marginHeight={0} style={{ border: 'none', overflow: 'hidden', width: 370, height: 333, borderRadius: 5 }} allowtransparency="true" />
           </div>
           <div className="mt-12">
             <BSCalendar />
